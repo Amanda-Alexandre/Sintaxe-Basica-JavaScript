e0977575bb0d53725e416ec1e7eb5a843165b79e
Amanda Alexandre <amandaalexandre87@outlook.com>
2022-07-14 04:32:02 -0300

    Boas praticas JS

---
+++ Aula 2 - Variaveis/script.js
@@ -0,0 +1,127 @@
+// tipos primitivos
+
+//Boleanos
+
+/*var vOuF = false;
+console.log(vOuF);
+console.log(typeof(vOuF));
+*/
+//Number
+
+/*var numeroQualquer = 1;
+console.log(numeroQualquer);
+console.log(typeof(numeroQualquer));
+*/
+//String
+/*var nome = 'Ola';
+console.log(nome);
+console.log(typeof(nome));
+*/
+//como declarar variavel
+/*
+var variavel = 'diana';
+variavel = 'martine';
+console.log(variavel);
+*/
+/*
+let variavel2 = 'diana';
+variavel2 = 'martine';
+console.log = (variavel2);
+*/
+
+/*
+const constante = 'diana';
+constante = 'martine';
+console.log(constante);
+
+*/
+
+/*
+var escopoGlobal = 'Global';
+console.log(escopoGlobal);
+function escopoLocal(){
+    let escopoLocalInterno = 'Local';
+    console.log(escopoLocalInterno);
+}
+escopoLocal();
+*/
+
+//Atribuição
+
+/*var atribuicao = 'Hugo';
+console.log(atribuicao)
+*/
+
+
+//Comparação
+/*var comparacao = '0'
+var comp = 0
+result = comparacao == comp
+console.log(result)
+*/
+//Comparação Identica
+/*var comparacao = '0'
+var comp = 0
+result = comparacao === comp;
+console.log(result)
+*/
+
+//operadores
+
+//adicao
+/*var adicao = 1 + 1;
+console.log(adicao);
+*/
+//subtracao
+/*var subtracao = 1 + 1;
+console.log(subtracao);
+*/
+
+//Multiplicação
+/*var multiplicacao = 2 * 3;
+console.log(multiplicacao)
+*/
+
+//divisao real
+/*var divisao = 6 / 3;
+console.log(divisao);
+*/
+
+//divisao inteira
+/*var divisaoInteira = 5 % 2;
+console.log(divisaoInteira)
+*/
+
+//potencia
+var potencia = 5 **2;
+console.log(potencia);
+
+//operadores relacionais
+
+var maiorQue = 5 > 2;
+console.log(maiorQue);
+
+
+var menorQue = 5 < 2;
+console.log(menorQue);
+
+var maiorIgual = 5 >= 2;
+console.log(maiorIgual);
+
+var menorIguar = 5 <= 2;
+console.log(menorIguar);
+
+
+// Operadores Lógicos
+
+var e = true && false;
+console.log(e)
+
+var ou = true || false
+console.log(ou)
+
+var nao = !false;
+console.log(nao)
+
+
+
